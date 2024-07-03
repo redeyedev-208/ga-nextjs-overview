26719272d9be35177f43c6574ce2c3001fc97c9c
gilaguilar <redeyedev.208@gmail.com>
2024-07-02 22:26:27 -0600

    setup nested layout, working on dynamic routing next

---
+++ app/drinks/page.js
@@ -0,0 +1,24 @@
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(url);
+  // throw error
+  if (!response.ok) {
+    throw new Error('Error fetching data');
+  }
+  const data = await response.json();
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
+  console.log(data);
+  return (
+    <div>
+      <h1 className='text-7xl'>Drinks Page</h1>
+    </div>
+  );
+};
+
+export default DrinksPage;
---
+++ app/tasks/page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TasksPage = () => {
+  return <div className='text-7xl'>Tasks Page</div>;
+};
+
+export default TasksPage;

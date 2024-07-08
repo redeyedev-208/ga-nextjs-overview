8846da864606003ddc5656306882ed5e5aa4cd16
gilaguilar <redeyedev.208@gmail.com>
2024-07-07 22:39:20 -0600

    adding forced const to make the static page dynamic to avoid unwanted side effect

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,7 +1,6 @@
-import TaskForm from '@/components/TaskForm';
 import TaskFormCustom from '@/components/TaskFormCustom';
 import TaskList from '@/components/TaskList';
-import React from 'react';
+export const dynamic = 'force-dynamic';
 
 const TasksPage = () => {
   return (

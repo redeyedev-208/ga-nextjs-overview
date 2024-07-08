33e50bcf4d3e12c64bae6a774193a2078cc21217
gilaguilar <redeyedev.208@gmail.com>
2024-07-07 18:20:38 -0600

    added delete funcitonality, and option to disable js on a page if needed

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-
-const DeleteForm = () => {
-  return <div>Delete Form</div>;
+import { deleteTask } from '@/utils/actions';
+const DeleteForm = ({ id }) => {
+  return (
+    <form action={deleteTask}>
+      <input
+        type='hidden'
+        name='id'
+        value={id}
+      />
+      <button className='btn btn-error btn-xs'>delete</button>
+    </form>
+  );
 };
 
 export default DeleteForm;
